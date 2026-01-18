0d66d5d0f56d8b560ae231a145360a37d1764a7e
AbhijeetGoril <abhijeetgorilz@gmail.com>
2026-01-18 14:05:52 +0530

    add axios dependency and create axios instance for API requests

---
+++ src/lib/axois.js
@@ -0,0 +1,8 @@
+// lib/axios.js
+import axios from "axios";
+const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:3000/api":"/api"
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,   // 🔥 MOST IMPORTANT — SEND COOKIES
+});
+
