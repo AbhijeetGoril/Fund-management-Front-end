5c1e62ee18fed0f31bc2b36bb7caa1dce2d30fa7
AbhijeetGoril <abhijeetgorilz@gmail.com>
2026-04-30 14:03:16 +0530

    refactor store configuration to include auth slice in the Redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import eventsReducer from "./slices/eventsSlice";
 import themeReducer from "./slices/themeSlice";
+import authSlice from "./slices/authSlice";
 
 export const store=configureStore({
   reducer:{
     events: eventsReducer,
-    theme: themeReducer
+    theme: themeReducer,
+    auth:authSlice
   }
 })
